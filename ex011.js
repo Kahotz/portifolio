8a0723817d922ab484ba994619207799d97b590c
Katrin Hotz <heron.nasser@hotmail.com>
2024-02-25 18:55:20 -0300

    Commit inicial

---
+++ ex011.js
@@ -0,0 +1,8 @@
+var idade = 69
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18 || idade >= 70) {
+    console.log('Voto facultativo')
+} else {
+    console.log('Voto obrigatório')
+}
